a5be4ffbc9fd1618b43fbac92748730c3bac7d05
dzenya-7 <69092908+dzenya-7@users.noreply.github.com>
2021-09-25 01:10:18 +0300

    Add files via upload

---
+++ src/main/java/com/example/sharedpreferences/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.sharedpreferences;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String PREFS_FILE =
+            "Account";
+    private static final String PREF_NAME = "Name";
+    SharedPreferences settings;
+    @Override
+    protected void onCreate(Bundle
+                                    savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        settings = getSharedPreferences(PREFS_FILE,
+                MODE_PRIVATE);
+    }
+    public void saveName(View view) {
+        // получаем введенное имя
+        EditText nameBox = (EditText)
+                findViewById(R.id.nameBox);
+        String name = nameBox.getText().toString();
+        // сохраняем его в настройках
+        SharedPreferences.Editor prefEditor =
+                settings.edit();
+        prefEditor.putString(PREF_NAME, name);
+        prefEditor.apply();
+    }
+    public void getName(View view) {
+        // получаем сохраненное имя
+        TextView nameView = (TextView)
+                findViewById(R.id.nameView);
+        String name =
+                settings.getString(PREF_NAME,"не определено");
+        nameView.setText(name);
+    }
+}
